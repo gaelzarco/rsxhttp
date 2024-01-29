6fa756ce7d6bec443273b3fdb0723fe73a41bc84
gael <xarco@macX.local>
2024-01-28 19:17:30 -0800

    handle_connection

---
+++ README.md
@@ -0,0 +1,7 @@
+## Demo web server built using Rust.
+
+##### This program follows the final project in the Rust Programming Language book.
+
+For more information, please read the free and publicly available Rust resources:
+
+- ![]
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
-    println!("Simple HTTP server built for Rust from scratch.");
     const HOST: &str = "127.0.0.1";
     const PORT: &str = "5001";
 
     let endpoint: String = HOST.to_owned() + ":" + PORT;
 
     // Listner for incoming TCP streams
+    // bind function returns an Error<T, E>
+    // It can fail because some ports require admin access
     let listener = TcpListener::bind(endpoint.clone()).expect("Endpoint is not valid");
 
     for stream in listener.incoming() {
-        let stream = stream.expect("Incoming stream dropped");
+        let stream = stream.expect("Stream could not be parsed");
 
-        println!("{endpoint} pinged!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }
