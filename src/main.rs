a37404aaa2cf40b3f18d95535389907edd701393
gael <xarco@macX.local>
2024-01-28 21:57:39 -0800

    single-threaded HTTP server in rust

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -23,11 +24,31 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
+    let request_line = buf_reader
         .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+        .next()
+        .expect("TcpStream iterator is empty")
+        .expect("Could not parse string");
 
-    println!("Request: {:#?}", http_request);
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 OK";
+        let contents = fs::read_to_string("src/hello.html").expect("Failed to open specified file");
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream
+            .write_all(response.as_bytes())
+            .expect("Could not write stream");
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("src/err.html").expect("Failed to open specified file");
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream
+            .write_all(response.as_bytes())
+            .expect("Could not write stream");
+    }
 }
