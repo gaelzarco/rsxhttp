2ef44d269603837a46b2d637c9997ba6780ad92f
Gael Zarco <gaelxarco@icloud.com>
2024-01-31 23:41:14 -0800

    implemented threads::JoinHandle instances vector

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
-    pub fn build(size: usize) -> ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
         /// Create a ThreadPool.
         ///
         /// The size is the number of threads in the pool.
@@ -11,7 +15,13 @@ impl ThreadPool {
         /// The `new` function will panic if the size is 0.
         assert!(size > 0);
 
-        ThreadPool
+        let threads = Vec::with_capacity(size);
+
+        for _ in 0..size {
+            // Create some threads and store them in a vector
+        }
+
+        ThreadPool { threads }
     }
 
     pub fn execute<F>(&self, f: F)
