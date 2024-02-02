39c039ccd82d705b04138eb8798342d508517378
Gael Zarco <gaelxarco@icloud.com>
2024-02-01 18:59:38 -0800

    completed multi-threaded web server

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,36 @@
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver
+                .lock()
+                .expect("Failed to acquire lock.")
+                .recv()
+                .expect("Could not receive job to execute.");
+
+            println!("Worker {id} got a job; Executing...");
+
+            job();
+        });
+
+        Worker { id, thread }
+    }
+}
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
@@ -15,18 +44,25 @@ impl ThreadPool {
         /// The `new` function will panic if the size is 0.
         assert!(size > 0);
 
-        let threads = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel();
 
-        for _ in 0..size {
-            // Create some threads and store them in a vector
+        let mut workers = Vec::with_capacity(size);
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { threads }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+
+        self.sender.send(job).expect("Could not send job.");
     }
 }
